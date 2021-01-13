410e5cfd9e13097c5a8bf5d931a1518d2ccbbbfa
SPrio <mahantasupriyo@gmail.com>
2021-01-13 10:36:16 +0530

    Added Test using Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+// setup file
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
 describe("Counter Testing", () => {
   test("render the title of counter", () => {
-    render(<App />);
-    const linkElement = screen.getByText('This is counter app');
-    expect(linkElement).toBeInTheDocument();
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').text()).toContain("This is counter app")
+
+    // const linkElement = screen.getByText('This is counter app');
+    // expect(linkElement).toBeInTheDocument();
   });
 });
