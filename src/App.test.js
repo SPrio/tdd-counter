82945f5571e27be0ddbc130c65c22d63032f1d36
SPrio <mahantasupriyo@gmail.com>
2021-01-13 11:30:35 +0530

    added beforeForEach with common code

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,27 @@
 // setup file
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import App from "./App";
 
 configure({ adapter: new Adapter() });
 
 describe("Counter Testing", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
   test("render the title of counter", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('h1').text()).toContain("This is counter app")
+    expect(wrapper.find("h1").text()).toContain("This is counter app");
 
     // const linkElement = screen.getByText('This is counter app');
     // expect(linkElement).toBeInTheDocument();
   });
 
   test("render a button with text of increment", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('#increment-btn').text()).toBe("Increment")
-  })
+    expect(wrapper.find("#increment-btn").text()).toBe("Increment");
+  });
+
+  test("render the initial value of state in a div", () => {
+    expect(wrapper.find('#counter-value').text()).toBe(0)
+  });
 });
