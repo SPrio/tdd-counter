a662277eda234351fd261d09a2b2a9aca604f3d9
SPrio <mahantasupriyo@gmail.com>
2021-01-13 10:46:02 +0530

    Added test for button Text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>This is counter app</h1>
+      <button id="increment-btn">Increment</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
 // setup file
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import App from "./App";
+
 configure({ adapter: new Adapter() });
 
 describe("Counter Testing", () => {
@@ -14,4 +13,9 @@ describe("Counter Testing", () => {
     // const linkElement = screen.getByText('This is counter app');
     // expect(linkElement).toBeInTheDocument();
   });
+
+  test("render a button with text of increment", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('#increment-btn').text()).toBe("Increment")
+  })
 });
