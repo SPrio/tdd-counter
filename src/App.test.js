d34e5de80c1d33b2ffb14d050ff8f6d4e5524713
SPrio <mahantasupriyo@gmail.com>
2021-01-17 09:39:55 +0530

    Applied formatter

--- src/App.test.js
+++ src/App.test.js
@@ -22,18 +22,18 @@ describe("Counter Testing", () => {
   });
 
   test("render the initial value of state in a div", () => {
-    expect(wrapper.find('#counter-value').text()).toBe("0")
+    expect(wrapper.find("#counter-value").text()).toBe("0");
   });
 
-  test('render the click event of increment button and increment counter value', () => {
+  test("render the click event of increment button and increment counter value", () => {
     wrapper.find("#increment-btn").simulate("click");
     expect(wrapper.find("#counter-value").text()).toBe("1");
-  })
+  });
 
-  test('render the click event of decrement button and decrement counter value', () => {
+  test("render the click event of decrement button and decrement counter value", () => {
     wrapper.find("#increment-btn").simulate("click");
     expect(wrapper.find("#counter-value").text()).toBe("1");
     wrapper.find("#decrement-btn").simulate("click");
     expect(wrapper.find("#counter-value").text()).toBe("0");
-  })
+  });
 });
